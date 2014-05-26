307e3d45cae0ef3a37b564db59c0e178592493aa
Artie Groove <apple2000@mail.ru>
2014-05-27 01:18:57 +0400

    Исправления последствий колиных тестовых логинов

--- www/upload/class.Upload.php
+++ www/upload/class.Upload.php
@@ -3,12 +3,8 @@ class Upload
 {
 	private $uploadPath = '/files/';
 	private $mimeFileType = array('xls' => 'application/vnd.ms-excel', 'xlsx' => 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-<<<<<<< HEAD
-	private $maxUploadFileSize = 1;
-=======
     //private $mimeFileType = array('xlsx' => 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
 	private $maxUploadFileSize = 1; // MB
->>>>>>> dcc729c92e37ef1ecbceca606271811e27a117a1
 	private $uploadFileName;
 	protected $rezult;
 	
@@ -30,20 +26,6 @@ class Upload
 			$this->setRezult('details', 'Файл не загружен на сервер.');
 			return false;
 		}
-<<<<<<< HEAD
-		
-		if ( strcmp($File['type'], $this->mimeFileType['xlsx'])!=0 )
-		{
-			$this->setRezult('status', 'Error');
-			$this->setRezult('details', 'Загруженый файл не соответствует формату xlsx.');
-			return false;
-		}
-		
-		if ( $File['size']>=($this->maxUploadFileSize*1024*1024) ) // размер файла >= 1Mb
-		{
-			$this->setRezult('status', 'Error');
-			$this->setRezult('details', 'Максимальный допустимый размер загружаемого файла 1 мегабайт.');
-=======
 
         // Функции серии finfo_* требуют особой настройки php:
         // во-первых, в php.ini нужно раскоментировать строку php_fileinfo.dll
@@ -65,7 +47,6 @@ class Upload
 		{
 			$this->setRezult('status', 'Error');
 			$this->setRezult('details', 'Максимально допустимый размер загружаемого файла 1 мегабайт.');
->>>>>>> dcc729c92e37ef1ecbceca606271811e27a117a1
 			return false;
 		}
 		
@@ -78,11 +59,7 @@ class Upload
 			return false;
 		}
 		
-<<<<<<< HEAD
-		$this->setRezult('status', 'Ok');
-=======
 		$this->setRezult('status', 'Файл успешно загружен на сервер');
->>>>>>> dcc729c92e37ef1ecbceca606271811e27a117a1
 		return true;
 	}
 }
